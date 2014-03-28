4f25f3e9e00ebde72442a005b92cde96e8f348c8
Guillermo Rivero <alu0100702149.ull.edu.es>
2014-03-28 19:59:39 +0000

    Cambios tests

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,7 +1,7 @@
 var assert = chai.assert;
 
 suite('Pruebas para el parser', function(){
-  test('Ejemplo de codigo para el parser', function(){
+  test('Asociacion a izquierdas', function(){
 	var input = "a = 3-2-1";
 	var resultado = pl0.parse(input);
 	var esperado = "{\n  \"type\": \"=\",\n  \"left\": {\n    \"type\": \"ID\",\n    \"value\": \"a\"\n  },\n  \"right\": {\n    \"type\": \"-\",\n    \"left\": {\n      \"type\": \"-\",\n      \"left\": {\n        \"type\": \"NUM\",\n        \"value\": 3\n      },\n      \"right\": {\n        \"type\": \"NUM\",\n        \"value\": 2\n      }\n    },\n    \"right\": {\n      \"type\": \"NUM\",\n      \"value\": 1\n    }\n  }\n}";
@@ -11,4 +11,29 @@ suite('Pruebas para el parser', function(){
 	assert.equal(esperado, resultado);
 
   });
+
+    test('Begin-End', function(){
+	var input = "begin\n	x = 1 + 2\n end.";
+	var resultado = pl0.parse(input);
+	var esperado = "...";
+	
+	resultado = JSON.stringify(resultado,undefined,2);
+
+	assert.equal(esperado, resultado);
+
+  });
+
+
+  test('Begin-End', function(){
+	var input = "begin\n	x = 1 + 2\n end.";
+	var resultado = pl0.parse(input);
+	var esperado = "...";
+	
+	resultado = JSON.stringify(resultado,undefined,2);
+
+	assert.equal(esperado, resultado);
+
+  });
 });
+
+
